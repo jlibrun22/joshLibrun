b09596cab0c9c11d269366f5f22b44a760f57d0a
Josh Librun <jlibrun@gmail.com>
2015-08-04 02:57:57 -0500

    added rotating blocks for resume page

--- test.js
+++ test.js
@@ -1,135 +1,202 @@
-$(document).ready(function(){
-
-var iterator = -1;
-//I should make this an object of template name THEN json object of event handlers
-var templateArray = ['profile','resume','brand'];
-
-var tmpl;
+$(document).ready(function() {
 
 
-$('#next-button').click(function(){
 
+    var current;
 
-nextSlide();
+    function rotate() {
 
-});
+        // This seems like a sucky way to do it, but you can't select by classes because they execute in order
 
+        if (current == 1) {
+            $("#block-1").removeClass().addClass("active");
+            $("#block-2").removeClass().addClass("non-active-top");
+            $("#block-3").removeClass().addClass("non-active-middle");
+            $("#block-4").removeClass().addClass("non-active-bottom");
+        } else if (current == 2) {
+            $("#block-1").removeClass().addClass("non-active-bottom");
+            $("#block-2").removeClass().addClass("active");
+            $("#block-3").removeClass().addClass("non-active-top");
+            $("#block-4").removeClass().addClass("non-active-middle");
+        } else if (current == 3) {
+            $("#block-1").removeClass().addClass("non-active-middle");
+            $("#block-2").removeClass().addClass("non-active-bottom");
+            $("#block-3").removeClass().addClass("active");
+            $("#block-4").removeClass().addClass("non-active-top");
+        }
+        else {
+            $("#block-1").removeClass().addClass("non-active-top");
+            $("#block-2").removeClass().addClass("non-active-middle");
+            $("#block-3").removeClass().addClass("non-active-bottom");
+            $("#block-4").removeClass().addClass("active");
+           
 
-$('#prev-button').click(function(){
+        }
 
-
-prevSlide();
-
-});
-
-
-var prevSlide =  function(){
-
-
-if(iterator != 0){
-   iterator--;  
-} else {
-iterator = templateArray.length-1;
-}
-
-//Move slide off the screen
-
-$('.slide')
-        .transition({ scale: 1 })
-        .transition({ x: 1500 },
-            function(){
-
-//Move slide from left to center and place template
-
-$('.slide')
-        .css({ x: -1500 })
-        .transition({ x: 0 })
-        .transition({ scale: 9 })
-        .html('' + tmpl);
-
- } );
-
-
-//Get slide template
-
-var slideTemplate = templateArray[iterator];
-
-$.ajax({
-    url: 'templates/'+slideTemplate+'.html',
-    type: 'get',
-    async: false,
-    success: function(html) {
-        tmpl = html;
     }
-});
 
 
 
-}
+    var iterator = -1;
+    //I should make this an object of template name THEN json object of event handlers
+    var templateArray = ['profile', 'resume', 'brand'];
+
+    var tmpl;
+
+
+    $('#next-button').click(function() {
+
+
+        nextSlide();
+
+    });
+
+
+    $('#prev-button').click(function() {
+
+
+        prevSlide();
+
+    });
+
+
+    var prevSlide = function() {
+
+
+        if (iterator != 0 && iterator != -1) {
+            iterator--;
+        } else {
+            iterator = templateArray.length - 1;
+        }
+
+        //Move slide off the screen
+
+        $('.slide')
+            .transition({
+                scale: 1
+            })
+            .transition({
+                    x: 1500
+                },
+                function() {
+
+                    //Move slide from left to center and place template
+
+                    $('.slide')
+                        .css({
+                            x: -1500
+                        })
+                        .transition({
+                            x: 0
+                        })
+                        .transition({
+                            scale: 9
+                        })
+                        .html('' + tmpl);
+
+                });
+
+
+        //Get slide template
+
+        var slideTemplate = templateArray[iterator];
+
+        $.ajax({
+            url: 'templates/' + slideTemplate + '.html',
+            type: 'get',
+            async: false,
+            success: function(html) {
+                tmpl = html;
+            }
+        });
+
+
+
+    }
 
 
 
 
-var nextSlide =  function(){
+    var nextSlide = function() {
 
-        if(iterator!=templateArray.length-1){
-        
-        iterator ++;
+        if (iterator != templateArray.length - 1) {
+
+            iterator++;
 
         } else {
-        iterator = 0;
+            iterator = 0;
         }
 
 
-//Get html for next slide
-var slideTemplate= templateArray[iterator];
+        //Get html for next slide
+        var slideTemplate = templateArray[iterator];
 
-$.ajax({
-    url: 'templates/'+slideTemplate+'.html',
-    type: 'get',
-    async: false,
-    success: function(html) {
-        tmpl = html;
+        $.ajax({
+            url: 'templates/' + slideTemplate + '.html',
+            type: 'get',
+            async: false,
+            success: function(html) {
+                tmpl = html;
+            }
+        });
+
+
+        //Move slide off the screen
+
+        $('.slide')
+            .transition({
+                scale: 1
+            })
+            .transition({
+                    x: -1500
+                },
+                function() {
+
+                    //Move slide from right to left
+
+                    $('.slide')
+                        .css({
+                            x: 1500
+                        })
+                        .transition({
+                            x: 0
+                        })
+                        .transition({
+                            scale: 9
+                        })
+                        .html('' + tmpl);
+
+                });
     }
-});
 
 
-//Move slide off the screen
+    /*Rotationg Blocks test*/
+    var rotateBlocks = function(newActive) {
 
-$('.slide')
-        .transition({ scale: 1 })
-        .transition({ x: -1500 },
-            function(){
+        $("#resume").find(".active-block").removeClass('move-center').addClass('move-away');
+        $(newActive).removeClass("move-away").addClass('move-center');
 
-//Move slide from right to left
+    }
 
-$('.slide')
-        .css({ x: 1500 })
-        .transition({ x: 0 })
-        .transition({ scale: 9 })
-        .html('' + tmpl);
 
-            });
+    //$('body').on('click','#resume section',rotateBlocks());
+    /*$('body').on('click','#resume section',function(event){
+        var el = ($(event.target).is('section'))? event.target : event.target.parentElement;
+       if($(el).hasClass('inactive-block') ){
+
+        rotateBlocks(el);
        }
+        
+    });*/
 
 
-/*Rotationg Blocks test*/
-var rotateBlocks = function(newActive){
-
-$("#resume").find( ".active-block" ).removeClass('move-center').addClass('move-away');
-$(newActive).removeClass("move-away").addClass('move-center');
-
-}
+    $('body').on('click', '#rotator div', function(event) {
+        // Enables reversing, idea via Andrea Canton: https://twitter.com/andreacanton/status/24954634279849985
+        current = this.id.substr(6);
+        rotate();
+    });
 
 
-//$('body').on('click','#resume section',rotateBlocks());
-$('body').on('click','#resume section',function(event){
-    var el = ($(event.target).is('section'))? event.target : event.target.parentElement;
-   if($(el).hasClass('inactive-block') ){
 
-    rotateBlocks(el);
-   }
-    
-});
 
 });
