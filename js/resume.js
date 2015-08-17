77e2343c32de4c61578883b7fdca74079889b063
Josh <jlibrun@gmail.com>
2015-08-17 04:41:56 -0500

    styling changes to the resume section

--- js/resume.js
+++ js/resume.js
@@ -44,24 +44,97 @@ require(['libs/avgrund/jquery.avgrund'], function() {
 
  $('body').on('click', '#rotator div', function(event) {
         // Enables reversing, idea via Andrea Canton: https://twitter.com/andreacanton/status/24954634279849985
-        current = this.id.substr(6);
-        rotate();
+       // if(!$(event.target).hasClass('active')){
+
+                    current = this.id.substr(6);
+                    rotate(); 
+        //}
+
+ 
     });
 
 $(function() {
         $('#show').avgrund({
-            height: 400,
-            holderClass: 'custom',
+            width: 640, // max is 640px
+            height: 350, // max is 350px
+            holderClass: 'modalDialog',
             showClose: true,
             showCloseText: 'close',
             onBlurContainer: '.container',
-            template: '<p>So implement your design and place content here! If you want to close modal, please hit "Esc", click somewhere on the screen or use special button.</p>' +
-            '<div>' +
-            '<a href="http://github.com/voronianski/jquery.avgrund.js" target="_blank" class="github">Avgrund on Github</a>' +
-            '<a href="http://twitter.com/voronianski" target="_blank" class="twitter">Twitter</a>' +
-            '<a href="http://dribbble.com/voronianski" target="_blank" class="dribble">Dribbble</a>' +
-            '</div>'
+            template: '<p>Details coming Soon.</p>'
         });
+
+          $('#show-first-rotation').avgrund({
+            width: 640, // max is 640px
+            height: 350, // max is 350px
+            holderClass: 'modalDialog',
+            showClose: true,
+            showCloseText: 'close',
+            onBlurContainer: '.container',
+            template: ' <h2>Customer Growth Initiative/Digital Innovation Group</h2>'
+             + '<p>Mobile/Web Analyst and Project Manager for our new digital solutions team at GE Capital Americas</p>'
+
+             + '<p>From vision to completion, participated in design, implementation, and rollout  of SmartChart web app</p>'
+
+            + '<p>Drove solution to capture metrics on iPad usage across GE Capital Americas for approximately 1000 iPad users in order to drive usage</p>' 
+        });
+
+
+            $('#show-second-rotation').avgrund({
+            width: 640, // max is 640px
+            height: 350, // max is 350px
+            holderClass: 'modalDialog',
+            showClose: true,
+            showCloseText: 'close',
+            onBlurContainer: '.container',
+            template: ' <h2>Digital Marketing Team</h2>'
+             + '<p>Native IOS Developer and Project Manager for GE Captial Retail Finance</p>'
+
+             + '<p>Developed 2 Native IOS Applications to be used internally within GE Capital Retail Finance</p>'
+
+            + '<p>Drove solution to capture metrics on iPad usage across GE Capital Americas for approximately 1000 iPad users in order to drive usage</p>' });
+
+
+              $('#show-third-and-fourth-rotation').avgrund({
+            width: 640, // max is 640px
+            height: 350, // max is 350px
+            holderClass: 'modalDialog',
+            showClose: true,
+            showCloseText: 'close',
+            onBlurContainer: '.container',
+            template: ' <h2> Enterprise Architecture Team - Software Engineering</h2>'
+             + '<p>Enterprise Architecture role with GE Capital Americas. </p>'
+
+             + '<p>Developed a variety of Web, Hybrid and Mobile applications from middle tier to front-end.  </p>'
+
+            + '<p>Taught mobile development workshop within GE as well as spoke at technical roadshow</p>'
+        });
+
+
+              $('#show-playspot').avgrund({
+            width: 640, // max is 640px
+            height: 350, // max is 350px
+            holderClass: 'modalDialog',
+            showClose: true,
+            showCloseText: 'close',
+            onBlurContainer: '.container',
+            template: '<p>COMING SOON PLAYSPOT INFO</p>' 
+
+        });
+
+            $('#show-dance-events').avgrund({
+            width: 640, // max is 640px
+            height: 350, // max is 350px
+            holderClass: 'modalDialog',
+            showClose: true,
+            showCloseText: 'close',
+            onBlurContainer: '.container',
+            template: '<p>COMING SOON DANCE EVENTS INFO</p>' 
+
+        });
+
+
+
     });
 
 
